77d6df9200f15c61419754901a9ba4a69b0d716d
Ra-Jo1 <jonatra1@gmail.com>
2026-02-18 17:15:39 +0100

    Update Prog_2_Faster_Launch_camera.py

--- Prog_2_Faster_Launch_camera/Prog_2_Faster_Launch_camera.py
+++ Prog_2_Faster_Launch_camera/Prog_2_Faster_Launch_camera.py
@@ -1,8 +1,15 @@
-#Just launch the camera faster that prog_1
+#Just launch the camera faster that prog_1 by controlling parameters, height, width, FPS and codec
 
 import cv2
 
-cam = cv2.VideoCapture(0)
+height = 360
+width = 640
+
+cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
+cam.set(cv2.CAP_PROP_FPS, 30)
+cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 
 while True:
     ignore,frame = cam.read()
