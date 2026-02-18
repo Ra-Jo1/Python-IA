9d1a405b83181db5b94c762186562f30cc8113fa
Ra-Jo1 <jonatra1@gmail.com>
2026-02-18 16:49:37 +0100

    Add camera capture functionality using OpenCV

---
+++ Prog_2_Faster_Launch_camera.py
@@ -0,0 +1,13 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ignore,frame = cam.read()
+
+    cv2.imshow("TheEnginneer",frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cam.release()
