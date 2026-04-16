2e6a6667e6cba989bf0aad639f407329b4661c80
Jo <jonatra1@gmail.com>
2026-04-16 15:12:04 +0200

    Update Prog_1_openCV_camera.py

--- Prog_1_camera/Prog_1_openCV_camera.py
+++ Prog_1_camera/Prog_1_openCV_camera.py
@@ -3,11 +3,11 @@ import cv2
 
 cam = cv2.VideoCapture(0)
 
-While True:
+while True:
   nothing,frame = cam.read()
   cv2.imshow("Yo_World",frame)
 
-  if waitKey(1) and 0xff == ord('q'):
+  if cv2.waitKey(1) & 0xff == ord('q'):
     break
 
 cam.release()
